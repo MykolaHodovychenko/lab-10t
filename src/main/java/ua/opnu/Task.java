b14d80fcdb8b0054bd800baaadf1ff21734bf1ae
Mykola Hodovychenko <nick.godov@gmail.com>
2022-10-30 14:38:29 +0200

    Initial commit

---
+++ src/main/java/ua/opnu/Task.java
@@ -0,0 +1,52 @@
+package ua.opnu;
+
+import java.util.*;
+
+public class Task {
+
+    public static void main(String[] args) {
+        // Для виконання лабораторної роботи необхідно написати вміст методів згідно умовам завдання,
+        // після чого протестувати метод за допомогою тестів, які знаходяться в папці
+        // src\test\TaskTest.java
+    }
+
+    public int maxLength(Set<String> set) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return 0;
+    }
+
+    public void removeEvenLength(Set<String> set) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+    }
+
+    public int numInCommon(List<Integer> list1, List<Integer> list2) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return 0;
+    }
+
+    public boolean isUnique(Map<String, String> map) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return false;
+    }
+
+    public Map<String, Integer> intersect(Map<String, Integer> map1, Map<String, Integer> map2) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return null;
+    }
+
+    public Map<String, Integer> reverse(Map<Integer, String> map) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return null;
+    }
+
+    public int rarest(Map<String, Integer> map) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return 0;
+    }
+
+    public int maxOccurrences(List<Integer> list) {
+        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
+        return 0;
+    }
+
+}
